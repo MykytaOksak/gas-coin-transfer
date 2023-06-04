535c41a37f2a08d2594810088c62cdc790a35557
Mykyta <maoksak.fitu19@kubg.edu.ua>
2023-06-04 16:01:51 +0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,32 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useAccount, usePrepareSendTransaction, useSendTransaction } from 'wagmi';
+import { utils } from 'ethers'
+import React from 'react';
+import { useDebounce } from 'use-debounce';
+import { parseEther } from 'viem';
 
 const Home: NextPage = () => {
+  const { isConnected } = useAccount();
+  const [to, setTo] = React.useState('0xd7c737a195f75ccb819a1ce7474892e4ab6bff8f')
+  const [debouncedTo] = useDebounce(to, 500)
+ 
+  const [amount, setAmount] = React.useState('0.0001')
+  const [debouncedAmount] = useDebounce(amount, 500)
+
+  const { config } = usePrepareSendTransaction({
+      to: debouncedTo,
+      value: debouncedAmount ? BigInt(utils.parseEther(debouncedAmount).toString()) : undefined,
+  })
+  const { sendTransaction } = useSendTransaction(config)
+
+  const handleTransfer = (n: number) => {
+    for (let i = 0; i < n; i++) {
+      sendTransaction?.()
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,64 +42,31 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <ConnectButton />
 
-        <h1 className={styles.title}>
-          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
-          <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="text-6xl	font-sans	 font-bold	mb-8 mt-8">
+          Gas Token Sender 
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://rainbowkit.com">
-            <h2>RainbowKit Documentation &rarr;</h2>
-            <p>Learn how to customize your wallet connection flow.</p>
-          </a>
-
-          <a className={styles.card} href="https://wagmi.sh">
-            <h2>wagmi Documentation &rarr;</h2>
-            <p>Learn how to interact with Ethereum.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://github.com/rainbow-me/rainbowkit/tree/main/examples"
-          >
-            <h2>RainbowKit Examples &rarr;</h2>
-            <p>Discover boilerplate example RainbowKit projects.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Next.js Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-          >
-            <h2>Next.js Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        {isConnected &&
+        <div className="grid grid-cols-4 gap-4">
+          <button onClick={() => handleTransfer(1)} className="transition-all bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-lg">
+          1 tx
+          </button>
+          <button onClick={() => handleTransfer(10)} className="transition-all bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-lg">
+          10 tx
+          </button>
+          <button onClick={() => handleTransfer(100)} className="transition-all bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-lg">
+          100 tx
+          </button>
+          <button onClick={() => handleTransfer(1000)} className="transition-all bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-lg">
+          1000 tx
+          </button>
         </div>
+        }
+        {!isConnected && <p className="text-2xl">Connect to the wallet to get started.</p>}
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
-          Made with ❤️ by your frens at 🌈
-        </a>
+          Ohhh I am seeending...
       </footer>
     </div>
   );
